73baf0a317d9161655415b6c9d5b3c846f7ce2d8
alexborsch <borsch_alex@icloud.com>
2020-01-16 21:48:12 +0200

    Settings file

---
+++ settings.php
@@ -0,0 +1,26 @@
+<?php
+/********************************************************
+*  Settings file
+*
+*  https://coderlog.top
+*  https://youtube.com/CoderLog
+********************************************************/
+
+define('MYSQL_SERVER', '');
+define('MYSQL_USER', '');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB', '');
+
+
+function db_connect(){
+	$connect = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB)
+		or die("Error: ".mysqli_error($connect));
+	if(!mysqli_set_charset($connect, "utf8mb4")){
+		print("Error: ".mysqli_error($connect));
+	}
+	return $connect;
+}
+
+$connect = db_connect();
+
+?>
